b477d4adbbfd1dc8700195c4ca4552f1260d89fe
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-29 15:47:26 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -7,3 +7,11 @@ def reverse_each_word(sentence)
   return solution.join(" ")
 end
 
+def reverse_each_word(sentence)
+  solution = []
+  words = sentence.split(" ")
+  words.each |sentence| do
+    soulution << sentence.reverse
+  end
+  return solution.join(" ")
+end
